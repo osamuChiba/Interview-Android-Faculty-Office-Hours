808a85ab7b70ce3c0e0bedcebc25ee9eb7b5851a
Kael Nevarez <knevarez@mdlive.com>
2025-04-22 19:17:49 -0600

    [correctOrder]: Added sorted list and sorted list with new york time

--- app/src/test/java/edu/wgu/faculty/office/hours/OfficeHoursTest.kt
+++ app/src/test/java/edu/wgu/faculty/office/hours/OfficeHoursTest.kt
@@ -5,15 +5,28 @@ import org.junit.Assert.*
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
+import edu.wgu.faculty.office.hours.misc.newYorkZoneId
+import edu.wgu.faculty.office.hours.misc.toString
+import edu.wgu.faculty.office.hours.misc.toZonedDateTime
 import edu.wgu.faculty.office.hours.models.OfficeHourUiState
+import edu.wgu.faculty.office.hours.models.ScheduleResponse
 import edu.wgu.faculty.office.hours.models.ShiftResponse
 import edu.wgu.faculty.office.hours.models.toUiState
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.OffsetTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class OfficeHoursTest {
     private val moshi = Moshi.Builder().build()
     private val listType =
         Types.newParameterizedType(List::class.java, ShiftResponse::class.java)
     private val adapter: JsonAdapter<List<ShiftResponse>> = moshi.adapter(listType)
+
     // Don't worry about !!
     private val officeHoursResponse = adapter.fromJson(officeHours)!!
 
@@ -33,6 +46,82 @@ class OfficeHoursTest {
         officeHours.forEach { officeHour ->
             println("${officeHour.dayOfWeekShort} ${officeHour.officeHour}")
         }
+    }
+
+    @Test
+    fun correctOrder() {
+        assertTrue(true)
+
+        val officeHours = toUiStates()
+
+        val sortedOfficeHours = mutableListOf<OfficeHourUiState>()
+
+        sortedOfficeHours.addAll(officeHours.sortedWith(compareBy { it.dayOfWeek.value }))
+        val sundays = sortedOfficeHours.filter { it.dayOfWeek.value == 7 }
+        sortedOfficeHours.removeAll(sundays)
+        sundays.forEach { sunday -> sortedOfficeHours.addFirst(sunday) }
+
+        sortedOfficeHours.toList().forEach { officeHour ->
+            println("${officeHour.dayOfWeekShort} ${officeHour.officeHour}")
+        }
+    }
+
+    @Test
+    fun correctOrderAndNewYorkTime() {
+        assertTrue(true)
+
+        val mutableHoursResponse = mutableListOf<ShiftResponse>()
+        val sortedOfficeHours = mutableListOf<OfficeHourUiState>()
+
+        officeHoursResponse.forEach { rawOfficeHour ->
+            val shiftResponse = ShiftResponse(
+                start = ScheduleResponse(
+                    day = rawOfficeHour.start?.day, time = convertToTimezone(
+                        hour = rawOfficeHour.start?.time ?: "",
+                        offset = rawOfficeHour.offset?.toInt() ?: 0
+                    )
+                ),
+                end = ScheduleResponse(
+                    day = rawOfficeHour.end?.day, time = convertToTimezone(
+                        hour = rawOfficeHour.end?.time ?: "",
+                        offset = rawOfficeHour.offset?.toInt() ?: 0
+                    )
+                ),
+                offset = rawOfficeHour.offset
+            )
+            mutableHoursResponse.add(shiftResponse)
+        }
+        sortedOfficeHours.addAll(mutableHoursResponse.mapNotNull { shift ->
+            shift.toOfficeHourEntity().toUiState()
+        }.sortedWith(compareBy { it.dayOfWeek.value }))
+
+        val sundays = sortedOfficeHours.filter { it.dayOfWeek.value == 7 }
+        sortedOfficeHours.removeAll(sundays)
+        sundays.forEach { sunday -> sortedOfficeHours.addFirst(sunday) }
+
+        sortedOfficeHours.toList().forEach { officeHour ->
+            println("${officeHour.dayOfWeekShort} ${officeHour.officeHour}")
+        }
+    }
+
+    /**
+     * Converts UTC hour string literal to
+     */
+    fun convertToTimezone(
+        hour: String,
+        timezone: ZoneId = newYorkZoneId,
+        offset: Int,
+        pattern: String = "HHmm"
+    ): String {
+        val formatter = DateTimeFormatter.ofPattern(pattern)
+        val localTime = LocalTime.parse(hour, formatter)
+        val localDateTime = LocalDateTime.of(LocalDate.now(), localTime)
+        val timeZoneOffset =
+            localDateTime.atOffset(ZoneOffset.UTC)
+        timeZoneOffset.withOffsetSameInstant(ZoneOffset.ofHours(offset))
+        val convertedDateTime =
+            localDateTime.atOffset(timeZoneOffset.offset).atZoneSameInstant(timezone)
+        return convertedDateTime.toString(pattern)
     }
 
     // 1) Fix the incorrect order shown above.
